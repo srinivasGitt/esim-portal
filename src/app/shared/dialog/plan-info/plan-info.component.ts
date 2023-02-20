6c5672dc51fb9f693deb4676773e534cd13bce09
Ankitp1696 <ankit.patil@penpenny.com>
2023-02-20 19:52:51 +0530

    Display country list on plan info popup

--- src/app/shared/dialog/plan-info/plan-info.component.ts
+++ src/app/shared/dialog/plan-info/plan-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { DialogComponent } from '../../service/dialog';
+import { RegionsService } from '../../service';
 
 @Component({
   selector: 'app-plan-info',
@@ -9,9 +10,11 @@ import { DialogComponent } from '../../service/dialog';
 export class PlanInfoComponent implements OnInit {
   dialogRef: DialogComponent;
   planDetails: any;
+  countryList!: Array<any>;
   
   constructor(
     private viewContainer: ViewContainerRef,
+    private regionService: RegionsService
   ) {
     const _injector = this.viewContainer.injector;
     this.dialogRef = _injector.get<DialogComponent>(DialogComponent);
@@ -19,9 +22,22 @@ export class PlanInfoComponent implements OnInit {
 
   ngOnInit(): void {
     this.planDetails = this.dialogRef.context.data;
+    this.fetchContriesByGroup();
   }
 
   close(): void {
     this.dialogRef.close.emit();
+  }
+
+  fetchContriesByGroup(){
+    this.regionService.getCountriesByGroup(1).subscribe(
+      (result : any) => {
+        this.countryList = result?.length > 0 ? result : [];
+      }
+    )
+  }
+
+  displayContryList(){
+    return this.countryList.slice(2).map((country) => country.name).join(', ');
   }
 }
