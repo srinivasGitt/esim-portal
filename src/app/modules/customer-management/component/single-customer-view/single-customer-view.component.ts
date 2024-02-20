7feeb24afc563c1321ec2e982302847b2c99b6fb
Ishwar Panchariya <127089058+ishwarkp17@users.noreply.github.com>
2024-02-20 18:21:07 +0530

    refactored

--- src/app/modules/customer-management/component/single-customer-view/single-customer-view.component.ts
+++ src/app/modules/customer-management/component/single-customer-view/single-customer-view.component.ts
@@ -27,7 +27,7 @@ export class SingleCustomerViewComponent implements OnInit {
   ngOnInit(): void {}
 
   editCustomer(customer: any) {
-    this.router.navigate(['customer/edit', '1']);
+    this.router.navigate(['customers/edit', '1']);
   }
 
   // Invite User
